2c474e88e13228fffe2a65c2378062dc4ff87acd
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-20 09:17:26 +0100

    update

---
+++ src/Programmation_defensive/Exo1/FizzBuzz.java
@@ -0,0 +1,24 @@
+package Programmation_defensive.Exo1;
+
+public class FizzBuzz {
+    public static String fizzBuzz(int i) {
+        String res = "";
+        res = Integer.toString(i);
+        if(i == 0){
+           res = "0";
+        }
+        else if(i % 3 == 0 && i % 5 == 0)
+        {
+            res = "Programmation_defensive.Collections.Exo1.FizzBuzz";
+        }
+        else if(i % 3 == 0)
+        {
+            res = "Fizz";
+        }
+        else if(i % 5 == 0)
+        {
+            res = "Buzz";
+        }
+        return res;
+    }
+}
---
+++ src/Programmation_defensive/Exo2/Ami.java
@@ -0,0 +1,17 @@
+package Programmation_defensive.Exo2;
+
+public class Ami {
+
+    public static String bonjour(String nom) {
+        String retNom = nom;
+        String retBonjour = "Bonjour";
+
+        if (nom == null) {
+            retNom = "Ami";
+        } else if(nom .equals(nom.toUpperCase())){
+            retBonjour = retBonjour.toUpperCase();
+            retNom = retNom.toUpperCase() ;
+        }
+        return retBonjour + ", " + retNom + ".";
+    }
+}
